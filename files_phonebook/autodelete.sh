e86534010d6d8ef9487fa82199dc228f946cf3b7
Shubhro Saha <shbhrsaha@gmail.com>
2013-12-21 22:58:00 -0500

    new autodelete

--- files_phonebook/autodelete.sh
+++ files_phonebook/autodelete.sh
@@ -3,6 +3,6 @@ for file in *;
     file_size=$(du $file | awk '{print $1}');
     if [ $file_size == 0 ]; then
         echo "Deleting empty file $file with file size $file_size!";
-        echo "rm -f $file";
+        rm -f $file;
     fi;
   done
